5a7bb34ce1b30ffaba04da818b985e858db579f6
Lauren Posey and Matthew Giem <lauren+matthew@>
2016-08-11 11:14:23 -0700

    allow a user to sell a pint and will update the number of pints left in the keg

--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -8,15 +8,20 @@ import { NewKegComponent } from './new-keg.component';
   inputs: ["kegList"],
   directives: [NewKegComponent],
   template: `
-    <keg-display *ngFor="#keg of kegList">Name of Keg: {{ keg.name }}, Alcohol Content: {{ keg.alcohol }}, Brewery: {{ keg.brand }}, Price of a Pint {{ keg.price }}, {{ keg.pints }} pints left
+    <keg-display *ngFor="#keg of kegList">Name of Keg: {{ keg.name }}, Alcohol Content: {{ keg.alcohol }}, Brewery: {{ keg.brand }}, Price of a Pint {{ keg.price }}, {{ keg.pints }} pints left <button (click)="sellPint(keg)">sell 1 pint</button>
     <br>
     </keg-display>
     <new-keg (onSubmitNewKeg)="addKegToList($event)"></new-keg>
+    <br>
   `
 })
 export class KegListComponent {
   public kegList: Keg[];
   addKegToList(newKeg: Keg): void {
     this.kegList.push(newKeg);
+  }
+  sellPint(keg: Keg): void {
+    console.log(keg);
+    keg.pints -=1;
   }
 }
