81ec47ac35a46dc3d09cdc9d3fdcead9c8cca838
Lauren Posey and Matthew Giem <lauren+matthew@>
2016-08-11 12:03:33 -0700

    add some functionality to the keg out put, if you have less than 10 it alerts you and changes the color of the text

--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -8,11 +8,12 @@ import { NewKegComponent } from './new-keg.component';
   inputs: ["kegList"],
   directives: [NewKegComponent],
   template: `
-    <keg-display *ngFor="#keg of kegList">Name of Keg: {{ keg.name }}, Alcohol Content: {{ keg.alcohol }}, Brewery: {{ keg.brand }}, Price of a Pint {{ keg.price }}, {{ keg.pints }} pints left <button (click)="sellPint(keg)">sell 1 pint</button>
+    <keg-display *ngFor="#keg of kegList"><div [style.color]="getStyle(keg)">Name of Keg: {{ keg.name }}</div><br> Alcohol Content: {{ keg.alcohol }}, Brewery: {{ keg.brand }}, Price of a Pint {{ keg.price }}, {{ keg.pints }} pints left <button (click)="sellPint(keg)">sell 1 pint</button>
     <br>
     </keg-display>
     <new-keg (onSubmitNewKeg)="addKegToList($event)"></new-keg>
     <br>
+    <money-made></money-made>
   `
 })
 export class KegListComponent {
@@ -22,6 +23,20 @@ export class KegListComponent {
   }
   sellPint(keg: Keg): void {
     console.log(keg);
-    keg.pints -=1;
+    if(keg.pints === 11)  {
+      alert("you're about to run out of " + keg.name);
+    }
+    if(keg.pints > 0) {
+      keg.pints -=1;
+    } else{
+      alert("out of beer");
+    }
+  }
+  getStyle(keg: Keg)  {
+    if(keg.pints < 10){
+      return "blue"
+    } else {
+      return "";
+    }
   }
 }
